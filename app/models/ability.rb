8fe8d78276b914a56970b7973bebe33448dc6cc6
Syaiful Sabril <syaiful.sabril@gmail.com>
2017-05-30 17:09:02 +0700

    fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       if user.role == "Admin"
         can :manage, :all
       else
-        # can :read, :all
+        can :send_feedback, Entry
       end
     #
     # The first argument to `can` is the action you are giving the user
