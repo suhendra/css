b0e4cc2d3192e33c0bf95a7da0d7fa402a3f3c33
Suhendra <idsuhendra@gmail.com>
2017-05-30 09:47:57 +0700

    login with username fixed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :login
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-
-  def login
-      @login || self.username || self.email
+  def email_required?
+    false
   end
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+  def email_changed?
+    false
   end
 
 end
