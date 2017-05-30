ef6ec3a0d6d1a2747d598c643d91afb8c1d8ea3d
Suhendra <idsuhendra@gmail.com>
2017-05-30 22:06:58 +0700

    optimized active record query and change to no jquery bootstrap.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,7 +6,7 @@ class EntriesController < ApplicationController
   # GET /entries
   # GET /entries.json
   def index
-    @counters = Counter.includes(:users, :entries)
+    @users = User.includes(:counter, :entries)
   end
 
   # GET /entries/1
